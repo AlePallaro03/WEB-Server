81c5b15f767eb1bc2aeb8dc284a002ca639b76de
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-03-29 08:47:25 +0000

    Root

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const express = require('express');
 const webServer = express();
 const port = 3000; //imposto la porta
 
-const router = require('./routes.js'); //richiamo il modulo router
-
-webServer.use('/', router);
-
 webServer.get('*',function(req, res){
-  res.send('Hello Get world!') //output con GET
+  res.send('hello Get world!')
 });
 
 webServer.post('*',function(req, res){
-  res.send('Hello Post world!') //output con POST
+  res.send('hello Post world!')
 });
-
 webServer.listen(port, function(){
   console.log(`Il programma ascolta alla porta: ${port}!`) //output della porta
 });
