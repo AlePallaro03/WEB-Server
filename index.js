ab247f4493616f3386f0e1cbde62ad04487fda22
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-03-29 08:38:40 +0000

    GET e POST

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const webServer = express();
 const port = 3000; //imposto la porta
 
-webServer.get('/',function(req, res){
-  res.send('Hello World!')
+webServer.get('*',function(req, res){
+  res.send('hello Get world!')
+});
+
+webServer.post('*',function(req, res){
+  res.send('hello Post world!')
 });
 
 webServer.listen(port, function(){
-  console.log(`Il programma ascolta alla porta: ${port}!`) //output 
+  console.log(`Il programma ascolta alla porta: ${port}!`) //output
 });
