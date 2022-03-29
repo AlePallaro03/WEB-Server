6671a72cbab07fa230045a601a469cdaa7d6426f
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-03-29 09:26:27 +0000

    HTML Fancy

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const webServer = express();
 const port = 3000; //imposto la porta
 
+const router = require('./routes.js'); //richiamo il modulo router
 
-webServer.get('*',function(req, res){
-  res.send('hello Get world!')
-});
+webServer.use('/', router);
 
-webServer.post('*',function(req, res){
-  res.send('hello Post world!')
-});
+webServer.use('/event', express.static('evento'));
 
 webServer.listen(port, function(){
   console.log(`Il programma ascolta alla porta: ${port}!`) //output
--- routes.js
+++ routes.js
@@ -2,16 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-   res.send('GET route on things.');
+   res.send('Hello GET world');
 });
 router.post('/', function(req, res){
-   res.send('POST route on things.');
+   res.send('Hello POST world');
 });
 
-router.post('/event',function(req, res){
-  res.send('evento/fancyParty.html')
-});
 
-//export this router to use in our index.js
-module.exports = router;
+
+module.exports = router; //esporto il modulo router
 
