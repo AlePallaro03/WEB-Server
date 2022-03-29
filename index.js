ab594c1d882b478c9d055b68726c12bc86df4177
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-03-29 08:45:08 +0000

    Aggiunta degli /Event

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const webServer = express();
 const port = 3000; //imposto la porta
 
+const router = require('./routes.js'); //richiamo il modulo router
+
+webServer.use('/', router);
+
 webServer.get('*',function(req, res){
-  res.send('hello Get world!')
+  res.send('Hello Get world!') //output con GET
 });
 
 webServer.post('*',function(req, res){
-  res.send('hello Post world!')
+  res.send('Hello Post world!') //output con POST
 });
 
 webServer.listen(port, function(){
-  console.log(`Il programma ascolta alla porta: ${port}!`) //output
+  console.log(`Il programma ascolta alla porta: ${port}!`) //output della porta
 });
