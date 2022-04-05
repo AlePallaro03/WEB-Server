b4536363708db4f475d4d7ba4115399550b8cc28
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-04-05 09:48:46 +0000

    Dynamic 2.0

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const port = 3000; //imposto la porta
 const router = require('./routes.js'); //richiamo il modulo router
 
 webServer.use('/', router);
-webServer.set('view engine', 'ejs');
+webServer.set('view engine', 'ejs'); //view engine
 webServer.use('/event', express.static('evento'));
 
-webServer.get('/evento/:views/dinamica', function(req, res) { //Pagina dinamica
-  res.render('dinamica.ejs', {evento: req.params.views});
+webServer.get('/evento', function(req, res) { //Pagina dinamica
+  res.render('dinamica'); //file dinamica situato in views/dinamica.ejs
 });
 
 webServer.listen(port, function(){
