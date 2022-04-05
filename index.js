75e25e7501a740557d97b122ab4f8a205c3a10b6
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-04-05 09:27:37 +0000

    Aggiunta Dynamic Page

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const port = 3000; //imposto la porta
 const router = require('./routes.js'); //richiamo il modulo router
 
 webServer.use('/', router);
-
+webServer.set('view engine', 'ejs');
 webServer.use('/event', express.static('evento'));
+
+webServer.get('/evento/:views/dinamica', function(req, res) { //Pagina dinamica
+  res.render('dinamica.ejs', {evento: req.params.views});
+});
 
 webServer.listen(port, function(){
   console.log(`Il programma ascolta alla porta: ${port}!`) //output
 });
-
 
 
