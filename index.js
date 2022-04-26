e6e6505bda6816233e52706e2d1a190d92e18e59
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-04-26 08:20:33 +0000

    comment 3.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express'); //richiesto modulo esterno
 const webServer = express();
 const port = 4000; //imposto la porta
 const ip = "https://replit.com/~" 
-console.log('\x1b[40mWEB SERVER') //output in console
+console.log('\x1b[40m\x1b[34mWEB SERVER') //output in console
 console.log('Realizzato Da')
 console.log('Alessandro Pallaro')
 const router = require('./router.js'); //richiedo il modulo router
@@ -32,7 +32,7 @@ webServer.get('*',function(req, res){
 });
 
 webServer.listen(port, function(){
-  console.log(`\x1b[40mIl programma ascolta alla porta: ${port}`)
+  console.log(`\x1b[40m\x1b[34mIl programma ascolta alla porta: ${port}`)
   console.log(`Il programma è in esecuzione su: ${ip}`)
 });
 
